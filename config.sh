b946f1e8c72dc97c0e2cd14f32d1d24f5a239b84
Ehsan Haider <Ehsan.Haider@panerabread.com>
2025-03-28 14:47:33 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ echo "
   \"listener\": [{ \"tcp\": { \"address\": \"[::]:8200\", \"tls_disable\": \"true\" } }],
   \"default_lease_ttl\": \"${DEFAULT_LEASE_TTL}\",
   \"max_lease_ttl\": \"${MAX_LEASE_TTL}\",
-  \"ui\": ${UI_ENABLED},
+  \"ui\": true,
   \"disable_mlock\": true
 }
 " > config.json
